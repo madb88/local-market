dd377bc040ade8ad55f14751026f7fbb6ae858f7
Jakub <kaminskiqba@gmail.com>
2024-01-26 08:51:24 +0100

    Working on theme mode button

--- app/components/ui/organisms/SearchBar.tsx
+++ app/components/ui/organisms/SearchBar.tsx
@@ -4,8 +4,9 @@ export default function SearchBar() {
 	return (
 		<div className="grid-cols-searchNavigation md:grid-cols-searchNavigationMid grid grid-rows-2 bg-slate-100 dark:bg-black md:grid-rows-1 md:px-10 md:py-5 md:pt-6">
 			<div className="">Logo</div>
-			<div className="order-3 col-span-2 md:order-2 md:col-span-1">SearchInput</div>
-			<div className="order-2 md:order-3">
+			<div className="order-4 col-span-2 md:order-2 md:col-span-1">SearchInput</div>
+			<div className="order-2 md:order-3">User Icons</div>
+			<div className="order-3 flex justify-end px-10">
 				<ModeToggle />
 			</div>
 		</div>
