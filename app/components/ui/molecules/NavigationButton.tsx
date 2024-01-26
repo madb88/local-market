e872b60df7ca764b9715698c1ed889d97693f161
Jakub <kaminskiqba@gmail.com>
2024-01-26 07:27:41 +0100

    Fixing type

--- app/components/ui/molecules/NavigationButton.tsx
+++ app/components/ui/molecules/NavigationButton.tsx
@@ -1,11 +1,11 @@
 import { FC } from "react";
 import { Button } from "../atoms/button";
 
-type NavigationButton = {
+type NavigationButtonType = {
 	children: string;
 };
 
-const NavigationButton: FC<NavigationButton> = ({ children }) => {
+const NavigationButton: FC<NavigationButtonType> = ({ children }) => {
 	return (
 		<Button size="sm">
 			<p>{children}</p>
