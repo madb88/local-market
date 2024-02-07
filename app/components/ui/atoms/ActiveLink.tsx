e921f6bacb0b15affb1471f5d66c7dfe91867e84
Jakub <kaminskiqba@gmail.com>
2024-02-07 09:41:03 +0100

    Update for active link

--- app/components/ui/atoms/ActiveLink.tsx
+++ app/components/ui/atoms/ActiveLink.tsx
@@ -5,27 +5,44 @@ import Link from "next/link";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
 import type { Route } from "next";
+import { Button } from "./button";
 
 type ActiveLinkProps = {
 	href: Route<string> | UrlObject;
 	children: React.ReactNode;
 	exact?: boolean;
+	button?: boolean;
 };
 
-export const ActiveLink = ({ href, children, exact = false }: ActiveLinkProps) => {
+export const ActiveLink = ({ href, children, exact = false, button = false }: ActiveLinkProps) => {
 	const pathname = usePathname();
 	const isActive = exact ? pathname === href : pathname.startsWith(href as Route);
 
 	const className = "text-white-500 hover:text-gray-900 w-full";
-	const activeClassName = "text-gray-900";
+	const activeClassName = "text-gray-900 border-b-2 border-gray-900";
+	const buttonClassName = "w-full";
 
 	return (
-		<Link
-			href={href}
-			className={clsx(className, isActive && activeClassName)}
-			aria-current={isActive ? "page" : undefined}
-		>
-			{children}
-		</Link>
+		<>
+			{button ? (
+				<Button variant={isActive ? "active" : "default"} className={buttonClassName}>
+					<Link
+						href={href}
+						className={buttonClassName}
+						aria-current={isActive ? "page" : undefined}
+					>
+						{children}
+					</Link>
+				</Button>
+			) : (
+				<Link
+					href={href}
+					className={clsx(className, isActive && activeClassName)}
+					aria-current={isActive ? "page" : undefined}
+				>
+					{children}
+				</Link>
+			)}
+		</>
 	);
 };
--- app/components/ui/molecules/NavigationButton/NavigationButton.tsx
+++ app/components/ui/molecules/NavigationButton/NavigationButton.tsx
@@ -7,14 +7,12 @@ import { ActiveLink } from "../../atoms/ActiveLink";
 const NavigationButton: FC<NavigationT> = ({ text, size, url, icon }) => {
 	return (
 		<>
-			<Button size={size} className="px-4">
-				<ActiveLink key={text} href={url}>
-					<span className="flex w-full gap-5">
-						<div className="">{icon}</div>
-						<div className="">{text}</div>
-					</span>
-				</ActiveLink>
-			</Button>
+			<ActiveLink key={text} href={url} button={true}>
+				<span className="flex w-full gap-5">
+					<div className="">{icon}</div>
+					<div className="">{text}</div>
+				</span>
+			</ActiveLink>
 		</>
 	);
 };
