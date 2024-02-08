f39fb32a3f070c4b37a48dca69746eab4f9e4ef9
Jakub <kaminskiqba@gmail.com>
2024-02-08 14:25:29 +0100

    Update BottomNavigationCategories.tsx

--- app/components/ui/organisms/BottomNavigation/NavigationElements/BottomNavigationCategories.tsx
+++ app/components/ui/organisms/BottomNavigation/NavigationElements/BottomNavigationCategories.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { List } from "lucide-react";
-import { Sheet, SheetTrigger, SheetContent } from "../../../atoms/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "../../../atoms/sheet";
 import Navigation from "../../Navigation";
 
 export default function BottomNavigationCategories() {
 	return (
 		<Sheet key={"bottom"}>
-			<SheetTrigger asChild>
+			<SheetTrigger asChild className="w-full">
 				<List />
 			</SheetTrigger>
 			<SheetContent side={"bottom"} className="flex w-screen justify-center">
