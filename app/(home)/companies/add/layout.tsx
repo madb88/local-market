145a23ba98aa60f59eb0c499e054751a7b4fedec
Jakub <kaminskiqba@gmail.com>
2024-02-16 17:34:18 +0100

    Update layout.tsx

--- app/(home)/companies/add/layout.tsx
+++ app/(home)/companies/add/layout.tsx
@@ -1,7 +1,7 @@
 export default function AddNewCompanyLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
-			<div className="h-screen w-full px-10 pt-3">
+			<div className="h-screen w-full px-5 pt-3">
 				<div className="h-6/6 rounded-md bg-slate-300 pb-5 shadow-md dark:bg-slate-900">
 					{children}
 				</div>
