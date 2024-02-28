6f08fb53addc02de51c1e07a3a8a101cc2c02d05
Jakub <kaminskiqba@gmail.com>
2024-02-28 16:32:35 +0100

    Fixes

--- app/components/pages/Offers/Functions/AddNewOfferButton.tsx
+++ app/components/pages/Offers/Functions/AddNewOfferButton.tsx
@@ -4,7 +4,7 @@ import { Button } from "@nextui-org/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function AddNewOfferButton({ href }: { href: string }) {
+export default function AddNewOfferButton({ href }: { href?: string }) {
 	const pathname = usePathname();
 
 	const link = href ? href : `${pathname}/add`;
