97358c54a9cbcffec552d73b12af7af7db0fcfc3
Jakub <kaminskiqba@gmail.com>
2024-03-18 10:02:07 +0100

    Update route.ts

--- app/api/cron/offers/route.ts
+++ app/api/cron/offers/route.ts
@@ -1,11 +1,18 @@
 import { revalidatePath, revalidateTag } from "next/cache";
+import { type NextRequest } from "next/server";
 import { deleteOffer } from "../../offers";
 
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
 
-export async function GET() {
-	const { message, status } = await deleteOffer(`${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!}`);
-	revalidateTag("userOffers");
-	revalidatePath("dashboard/offers");
-	return new Response(`${message} ${status}`);
+export async function GET(request: NextRequest) {
+	const token = request.nextUrl.searchParams.get("token");
+
+	if (token === String(process.env.CRON_TOKEN)) {
+		const { message, status } = await deleteOffer(`${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!}`);
+		revalidateTag("userOffers");
+		revalidatePath("dashboard/offers");
+		return new Response(`${message} ${status}`);
+	}
+
+	return new Response(`Not authorized`);
 }
