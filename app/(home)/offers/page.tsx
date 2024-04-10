2a4fb77d140117b0622b766d6a7a7310b5749d76
Jakub <kaminskiqba@gmail.com>
2024-04-10 08:34:27 +0200

    Categories page

--- app/(home)/offers/page.tsx
+++ app/(home)/offers/page.tsx
@@ -1,3 +1,23 @@
+import { categories } from "@/app/utils/categoriesData";
+import { Card, CardBody } from "@nextui-org/react";
+import Link from "next/link";
+
 export default async function CategoryPage() {
-	return <>Główna strona kategorii</>;
+	return (
+		<div className="flex  p-5">
+			<ul className="space-y-5">
+				{categories.map((category) => {
+					return (
+						<li key={category.key}>
+							<Card>
+								<CardBody>
+									<Link href={`offers/${category.value}`}>{category.label}</Link>
+								</CardBody>
+							</Card>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 }
