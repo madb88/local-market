9dd52149be7875b4b8ef9115cac67bd7a071a3b7
Jakub <kaminskiqba@gmail.com>
2024-04-18 12:16:26 +0200

    update layout

--- app/(home)/offers/[categoryName]/[id]/layout.tsx
+++ app/(home)/offers/[categoryName]/[id]/layout.tsx
@@ -1,7 +1,7 @@
 export default function OfferLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
-			<div className="h-screen w-full pt-3 md:px-5">
+			<div className="h-full w-full pt-3 md:px-5">
 				<div className="h-fit rounded-md bg-slate-200 pb-10 shadow-md dark:bg-slate-800">
 					{children}
 				</div>
